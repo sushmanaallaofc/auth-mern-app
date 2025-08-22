0f9fdb3ac657dced4b916dafb926ca468b5caa5a
sushmanaallaofc <97525523+sushmanaallaofc@users.noreply.github.com>
2025-08-22 11:30:54 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = `http://localhost:8000/auth/login`;
+            const url = `https://auth-mern-app-backend-23sv.onrender.com/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
@@ -89,5 +89,6 @@ function Login() {
         </div>
     )
 }
+
 
 export default Login
