b553718a5d2922c8caded526f88eb8150df85264
sushmanaallaofc <97525523+sushmanaallaofc@users.noreply.github.com>
2025-08-22 11:31:15 +0530

    Update SignUp.js

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -27,7 +27,7 @@ function Signup() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = `http://localhost:8000/auth/signup`;
+            const url = `https://auth-mern-app-backend-23sv.onrender.com/auth/signup`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
@@ -97,5 +97,6 @@ function Signup() {
         </div>
     )
 }
+
 
 export default Signup
