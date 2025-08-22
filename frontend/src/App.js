f4598c938c66b4b4ad9763875c1da0fa7248004c
sushmanaallaofc <97525523+sushmanaallaofc@users.noreply.github.com>
2025-08-22 16:40:14 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,25 +3,25 @@ import './App.css';
 import Login from './pages/Login';
 import Signup from './pages/SignUp';
 import Home from './pages/Home';
-import { useState } from 'react';
-import RefrshHandler from './RefrshHandler';
+import { AuthProvider, AuthContext } from './contexts/AuthContext'; 
+import { useContext } from 'react';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
   const PrivateRoute = ({ element }) => {
-    return isAuthenticated ? element : <Navigate to="/login" />
-  }
+    const { isAuthenticated } = useContext(AuthContext);
+    return isAuthenticated ? element : <Navigate to="/login" />;
+  };
 
   return (
     <div className="App">
-      <RefrshHandler setIsAuthenticated={setIsAuthenticated} />
-      <Routes>
-        <Route path='/' element={<Navigate to="/login" />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/home' element={<PrivateRoute element={<Home />} />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path='/' element={<Navigate to="/login" />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/home' element={<PrivateRoute element={<Home />} />} />
+        </Routes>
+      </AuthProvider>
     </div>
   );
 }
